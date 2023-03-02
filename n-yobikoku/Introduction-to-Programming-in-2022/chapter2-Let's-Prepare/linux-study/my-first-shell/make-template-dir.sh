71328ff6444eeeec4bda2cd0cd7f6d644b31a8b9
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-03-02 22:41:48 +0900

    add chapter2 lesson8

---
+++ n-yobikoku/Introduction-to-Programming-in-2022/chapter2-Let's-Prepare/linux-study/my-first-shell/make-template-dir.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "テンプレートのディレクトリ名を入力してください" dirName
+mkdir $dirName
+mkdir $dirName/1
+mkdir $dirName/2
+mkdir $dirName/3
--- n-yobikoku/Introduction-to-Programming-in-2022/chapter2-Let's-Prepare/linux-study/my-first-shell/my-first.sh
+++ n-yobikoku/Introduction-to-Programming-in-2022/chapter2-Let's-Prepare/linux-study/my-first-shell/my-first.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ls
 date
+echo メッセージを入力して下さい。
+read messeage
+echo 入力されたメッセージ： $messeage
